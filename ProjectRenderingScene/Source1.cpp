c5f797a6e5a8a7254193f6bd0a27b5a3948f18c2
Enrique Perez Soler <quiqueperezsoler@gmail.com>
2018-08-22 01:31:19 +0200

    Update files in the project adn add executable

--- ProjectRenderingScene/Source1.cpp
+++ ProjectRenderingScene/Source1.cpp
@@ -403,6 +403,7 @@ void DrawPolygonRows(const vector<Pixel>& leftPixels, const vector<Pixel>& right
 void DrawPolygon(const vector<Vertex>& vertices)
 {
 	int V = vertices.size();
+
 	vector<Pixel> vertexPixels(V);
 	for (int i = 0; i<V; ++i) {
 		VertexShader(vertices[i], vertexPixels[i]);
